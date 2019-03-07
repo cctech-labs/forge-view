2133af93d792e3d47dcfa5ddf09b83ccb516d9de
Nem Kumar <nemkumar@Mac-mini.local>
2019-03-07 18:18:46 +0530

    upgraded to polymer 3. GuiViewer3D not working

--- src/forge-view.js
+++ src/forge-view.js
@@ -1,6 +1,6 @@
-import { PolymerElement } from '../../@polymer/polymer/polymer-element.js';
-import { setPassiveTouchGestures } from '../../@polymer/polymer/lib/utils/settings.js';
-import { html } from '../../@polymer/polymer/lib/utils/html-tag.js';
+import { PolymerElement } from '../node_modules/@polymer/polymer/polymer-element.js';
+import { setPassiveTouchGestures } from '../node_modules/@polymer/polymer/lib/utils/settings.js';
+import { html } from '../node_modules/@polymer/polymer/lib/utils/html-tag.js';
 // TODO_NOP: hacky hardcode specific for making it as a polymer component
 window.LMV_RESOURCE_ROOT = "https://developer.api.autodesk.com/derivativeservice/v2/viewers/";
 // Gesture events like tap and track generated from touch will not be
@@ -10,8 +10,7 @@ setPassiveTouchGestures(true);
 class ForgeView extends PolymerElement {
   static get template() {
     return html`
-        <link rel="stylesheet" href="https://developer.api.autodesk.com/derivativeservice/v2/viewers/style.min.css?v=4.1" type="text/css">
-
+        <link rel="stylesheet" href="https://developer.api.autodesk.com/modelderivative/v2/viewers/6.*/style.min.css" type="text/css">
         <style>
             #viewerdiv {
                 width: 100%;
